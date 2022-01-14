85fb07f198b878bac9b533fbc710e9a85d27e874
jagodasad <jago.sadowska@gmail.com>
2022-01-14 13:51:23 +0000

    commit

--- test/test.go
+++ test/test.go
@@ -10,6 +10,8 @@ func main() {
 	piscine.BTreeInsertData(root, "1")
 	piscine.BTreeInsertData(root, "7")
 	piscine.BTreeInsertData(root, "5")
-	min := piscine.BTreeMin(root)
-	fmt.Println(min.Data)
+	node := piscine.BTreeSearchItem(root, "1")
+	replacement := &piscine.TreeNode{Data: "3"}
+	root = piscine.BTreeTransplant(root, node, replacement)
+	piscine.BTreeApplyInorder(root, fmt.Println)
 }
